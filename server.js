fae1eaf865b4e9afdbbbd5a8f49a165a1bf0633f
Agamenon Sousa Pereira <agamenon.bereana@hotmail.com>
2020-04-09 20:51:46 -0300

    Alterado server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.status(200).send({messsage: 'Olá mundo - tudo funcionando'});
+});
+
+app.listen(3001, () => {
+    console.log('api rodando na porta 3001');
+});
