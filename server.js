9d51099a6cea2618ac20944d691bf4fbb46f2eb3
Agamenon Sousa Pereira <agamenon.bereana@hotmail.com>
2020-04-10 08:17:18 -0300

    atualizei o server com nova rota

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ app.get('/', (req, res) => {
     res.status(200).send({messsage: 'Olá mundo - tudo funcionando'});
 });
 
+app.get('/ola', (req, res) => {
+    res.status(200).send({messsage: 'agora vou responder olá'});
+});
+
 app.listen(3001, () => {
     console.log('api rodando na porta 3001');
 });
+
 
 // Alterado arquivo server;
 // alterado no servidor que sera comitado no client;
